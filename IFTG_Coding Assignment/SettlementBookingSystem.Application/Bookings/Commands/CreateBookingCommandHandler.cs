aff97784092c4295cd2d07a0f73e0c1bec4858d9
nguyendung1911 <nguyendung19111986@gmail.com>
2022-08-20 16:52:44 +0700

    Add files via upload
    create test

---
+++ IFTG_Coding Assignment/SettlementBookingSystem.Application/Bookings/Commands/CreateBookingCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using SettlementBookingSystem.Application.Bookings.Dtos;
+using SettlementBookingSystem.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SettlementBookingSystem.Application.Bookings.Commands
+{
+    public class CreateBookingCommandHandler : IRequestHandler<CreateBookingCommand, BookingDto>
+    {
+        private readonly IMemoryCache _memoryCache;
+        public CreateBookingCommandHandler(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public Task<BookingDto> Handle(CreateBookingCommand request, CancellationToken cancellationToken)
+        {
+            if (!DateTime.TryParse(request.BookingTime, out DateTime bookingTime))
+            {
+                throw new Exception(); 
+            }
+
+            var hour = bookingTime.Hour;
+            if (hour < 9 || hour > 16)
+                throw new ValidationException("invalid time");
+
+            var currentBookings = _memoryCache.Get<List<string>>("booking") ?? new List<string>();
+
+            int numberOfConflict = 0;
+
+            if (currentBookings != null && currentBookings.Any())
+            {
+                foreach (var booking in currentBookings)
+                {
+                    var startBooking = DateTime.Parse(booking);
+                    var endBooking = startBooking.AddMinutes(59);
+
+                    if (bookingTime >= startBooking && bookingTime <= endBooking)
+                        numberOfConflict++;
+                }
+            }
+
+            if (numberOfConflict >= 4)
+                throw new ConflictException("Conflict Booking");
+
+            currentBookings.Add(bookingTime.TimeOfDay.ToString());
+            _memoryCache.Set("booking", currentBookings);
+
+            return Task.FromResult(new BookingDto());
+        }
+    }
+}
---
+++ IFTG_Coding Assignment/SettlementBookingSystem/Controllers/BookingController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SettlementBookingSystem.Application.Bookings.Commands;
+using SettlementBookingSystem.Application.Bookings.Dtos;
+using SettlementBookingSystem.Application.Exceptions;
+using System;
+using System.Threading.Tasks;
+
+namespace SettlementBookingSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookingController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BookingDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<BookingDto>> Create([FromBody] CreateBookingCommand command)
+        {
+            try
+            {
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (Exception ex) when (ex.Message == "Conflict Booking")
+            {
+                return Conflict(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
